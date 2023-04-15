2060dedce77c1b5c36b6145e82f226a3ff6d3c50
livingsneaky-art <ryanllamido@gmail.com>
2023-04-15 16:12:02 +0800

    Week 1 notes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Build Wireframes and Low-Fidelity Prototypes
  
+# SKILLS GAIN
+- 
+- 
+- 
+- 
+# Contributors
+LLAMIDO, RYAN B.
+
+# Project Status
+Ongoing(Week-1) - 04/15/2023<br>
+(Week-2) - 04//2023<br>
+(Week-3) - 04//2023<br>
--- week_1.js
+++ week_1.js
@@ -1,4 +1,91 @@
 //Storyboarding and wireframing
+/*
+    Welcome to the world of wireframes! You'll start by learning how to use research findings to inform ideation during the design process. 
+    Next, you'll create two types of storyboards: big picture and close-up. Then, you'll draw your first wireframes, and you'll explore the 
+    benefits of wireframing. Keep in mind that at this point in the design process, you should have lots of ideas for designs that address 
+    real user needs. You want to refine those ideas, and wireframing can help you do that.
+
+    Learning Objectives
+    - Build a goal statement
+    - Outline a user flow
+    - Create big picture and close-up storyboards, and understand when to use each type
+    - Explain the difference between low-fidelity and high-fidelity design
+    - Describe the purpose of wireframing in the design process
+    - Apply the basics of drawing
+*/
+
+//Introduction to Course 3
+//Introduction to Course 3: Build Wireframes and Low-Fidelity Prototypes
+/*
+    - Use research to inform the ideation process
+    - Build storyboards and explain how storyboards are used to create wireframes
+    - Draw basic elements
+    - Understand information architecture
+    - Create paper wireframes
+    - Build digital wireframes on a tool called Figma
+    - Construct low-fidelity paper prototypes
+    - Develop low-fidelity digital prototypes on Figma
+    - Understand how to avoid the effects of deceptive patterns in design
+*/
+
+//Welcome to Course 3
+/*
+    - Week 1: Storyboarding and wireframing
+    - Week 2: Creating paper and digital wireframes
+    - Week 3: Building low-fidelity prototypes
+*/
+
+//Optional - Read this if it's your first course in the certificate program
+
+//Optional - Test your knowledge of prior concepts
+
+//Optional - Interpret your quiz results
+
+//Optional - Submit your choice of portfolio project prompt
+
+//Karen - My path to UX design
 
 
+//User research to inform ideation
+//Welcome to week 1
 
+//Use research to inform ideation
+/*
+    Personas
+        - Fictional users whose goals and characteristics represent the needs of a larger group of users
+    User story
+        - Fictional one-sentence story told form a persona's point of view to inspire and inform design decisions
+    User journey
+        - The series of experience a user has as they interact with your product
+
+
+    
+*/
+
+//Optional - Learn more about how research inform ideation
+/*
+    - Empathy maps = explore users’ four main motivations: what the user says, thinks, does, and feels. The insights gathered from 
+        empathy maps help you come up with ideas for solutions that address the user’s real problems. 
+
+    - Personas = place the users who you’re designing for front-and-center. By creating detailed user profiles, you can clearly 
+        envision potential users that you’d design for.
+
+    - User stories = determine which user needs are the most critical to address with your designs. This direction will help focus your ideation.
+
+    - User journeys = help you come up with ideas for designs that truly support the users’ needs and solve their problems. 
+
+    - Problem statement = is a clear description of the user’s need that should be addressed. The problem statement you created in 
+        the last course will guide the focus of your ideation. 
+*/
+
+//Revisit problem statements
+/*
+    Problem statement
+        - [Username] is a/an [user characteristic] who needs [user need] because [insight].
+*/
+
+//Craig - Research informs ideation in the real world
+/*
+    - "What research should we be doing?"
+    - "Where can we have real benefit and impact to the business?"
+*/
